7ba595a057a98552ac6495f88637ac7639f3b828
Anthony Kowalski <anthonyrkowalski@gmail.com>
2020-08-17 16:08:27 -0700

    Updated routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Require controller modules.
+// var messages_controller = require('../controllers/messagesController');
+var user_controller = require('../controllers/userController');
+
+
+/// BOOK ROUTES ///
+
+// GET catalog home page.
+router.get('/user', user_controller.get_user_list);
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var user_controller = require('../controllers/userController');
+
+
+/// USER ROUTES ///
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/user', user_controller.get_user_list);
 
 module.exports = router;
