c9d9bdb9cce77f9d9aa515ea18014bbc985ca05a
Anthony Kowalski <anthonyrkowalski@gmail.com>
2020-08-22 10:49:06 -0700

    Add message route handler and new post link

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ db.on("error", console.error.bind(console, "mongo connection error"));
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const messagesRouter = require('./routes/messages');
 
 // Create the Express application object
 const app = express();
@@ -84,6 +85,7 @@ app.use(function(req, res, next) {
 // Set up Route Handling
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/messages', messagesRouter);
 
 // Log in || Log out //
 app.post(
