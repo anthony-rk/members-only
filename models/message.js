967522fba41bea6952e43e5e8cbdfed8713526ac
Anthony Kowalski <anthonyrkowalski@gmail.com>
2020-08-24 14:01:12 -0700

    Add message_title field to the message model

--- models/message.js
+++ models/message.js
@@ -6,8 +6,9 @@ var Schema = mongoose.Schema;
 
 var MessageSchema = new Schema(
   {
+    message_title: {type: String, required: true, maxlength: 150},
     message_text: {type: String, required: true, maxlength: 150},
-    user_name: {type: String, required: true, maxlength: 100},
+    username: {type: String, required: true, maxlength: 100},
     // date_of_post: {type: Date},
   }
 );
@@ -17,19 +18,8 @@ MessageSchema
   .virtual('date_of_post_formatted')
   .get(function () {
       var now = moment();
-
-    if (this.date_of_post) {
       return moment(now).format('MMMM Do, YYYY');
-    }
-    else return 'N/A';
 });
 
-// Virtual for author's URL
-// MessageSchema
-//   .virtual('url')
-//   .get(function () {
-//     return '/catalog/author/' + this._id;
-// });
-
-//Export model
+// Export model
 module.exports = mongoose.model('Message', MessageSchema);
