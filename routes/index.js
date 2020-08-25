6a7d2e85d8089c61a420577a2b814b9f970c194e
Anthony Kowalski <anthonyrkowalski@gmail.com>
2020-08-25 15:33:08 -0700

    Update route handlers

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var message_controller = require('../controllers/messagesController');
-
 /* GET home page. */
 router.get("/", (req, res) => {
-  // res.render("index", {message_controller.get_message_list, user: req.user});
-  res.render("index", { title: 'Message Board', user: req.user,  message_list: message_controller.get_message_list, });
+  res.render("index", { title: 'Message Board', user: req.user });
 });
 
 module.exports = router;
--- routes/messages.js
+++ routes/messages.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 var message_controller = require('../controllers/messagesController');
 
+/* GET messages for the /messages page. */
+router.get('/', message_controller.get_message_list);
+
 /* GET new post page. */
 router.get('/new-post', message_controller.message_create_get);
 
 /* POST new post page. */
 router.post('/new-post', message_controller.message_create_post);
-
-/* GET Messages Page */
-// router.get('/messages', message_controller.message_create_get);
 
 module.exports = router;
