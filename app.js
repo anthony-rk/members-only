02b4ab9be6c1030316c1ead694973193a5086bdd
Anthony Kowalski <anthonyrkowalski@gmail.com>
2020-09-07 11:46:29 -0700

    Update to get ready for production

--- app.js
+++ app.js
@@ -6,21 +6,19 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
 
 // Added the following 
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 const bcrypt = require("bcryptjs");
 const User = require('./models/user');
 
-const { body,validationResult } = require('express-validator/check');
-const { sanitizeBody } = require('express-validator/filter');
-
 // Setup MongoDB connection
-const mongoDb = process.env.DB_HOST;
+const mongoDb = process.env.MONGODB_URI;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true});
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
@@ -41,6 +39,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression()); // Compress all routes
+app.use(helmet());
 
 // PassportJS Authentication below
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
@@ -104,46 +104,6 @@ app.get("/users/log-out", (req, res) => {
   req.logout();
   res.redirect("/");
 });
-
-// Remove this once working
-// Remove this once working
-// Remove this once working
-// app.post("/users/become-member", (req, res, next) => {
-//     console.log(req.user.username)
-//     // Validate fields.
-//     body('secret_password').isLength({ min: 1 }).trim().withMessage('Password must be specified.'),
-    
-//     // Sanitize fields.
-//     sanitizeBody('secret_password').escape(),
-    
-//     // Check if input is equal to the secret word
-//     // If so then set User membership status to Member
-//     // Process request after validation and sanitization.
-//     (req, res, next) => {
-//         // Extract the validation errors from a request.
-//         const errors = validationResult(req);
-
-//         if (!errors.isEmpty()) {
-//             // There are errors. Render form again with sanitized values/errors messages.
-//             res.render('become_a_member', { title: 'Become a Member', user: req.user, errors: errors.array()});
-//             return;
-//         }
-//         else {
-//             // Data from form is valid.
-//             User.findOneAndUpdate({username: req.user.username },  
-//                 {membership_status:"Member"}, null, function (err) { 
-//                 if (err) { 
-//                     console.log(err)
-//                     return next(err)
-//                 } 
-//                 res.redirect("/messages");
-//             }); 
-//           }
-//     }
-//   })
-// Remove this once working
-// Remove this once working
-// Remove this once working
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
